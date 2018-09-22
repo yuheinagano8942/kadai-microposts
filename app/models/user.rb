bfb3dd4e5a85857354b7b1c9a1b58e81ab155592
yuhei.nagano <yuheinagano8942@gmail.com>
2018-09-22 07:39:54 +0000

    for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
-  has_many :favorites, dependent:destroy
-  has_many :favorites_microposts, through: :favorites, source: :micropost, dependent:destroy
+  has_many :favorites
+  has_many :favorites_microposts, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
